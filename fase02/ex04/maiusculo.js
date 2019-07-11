59b22945b0112fc0c033055d4fcf13e0f48c8ffe
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-07-11 20:48:18 -0300

    AAAtop

--- fase02/ex04/maiusculo.js
+++ fase02/ex04/maiusculo.js
@@ -1,4 +1,4 @@
-function maiusculo2 (palavra){
+function maiusculo (palavra){
     var palavra1 = palavra.substr(0,3);
     palavra = palavra.replace(palavra1, palavra1.toUpperCase());
     return palavra;

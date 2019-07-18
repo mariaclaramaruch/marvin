086423c800bd1137a65b4b3118bf583e93a572e4
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-07-18 20:55:53 -0300

    nove ULTIMO 9

---
+++ fase04/ex01/nove.js
@@ -0,0 +1,13 @@
+
+function nove(num) {
+    var numero= num.toString()
+    var ultima= numero.substr(-1,1)
+    if (ultima == '9' ){ 
+        return ("true");
+    } else { 
+        return ("false");
+} 
+} 
+
+
+

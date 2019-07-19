2dd8ae81352388897d27e0be6fc05258a53d27d9
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-07-18 21:56:41 -0300

    ABCdario

---
+++ fase04/ex04/abc.js
@@ -0,0 +1,13 @@
+
+function abc(palavra) {
+    var letra=palavra.toString()
+    var inicial= letra.substr(0,1)
+    if (inicial == "a"|| inicial == "b" || inicial == "c") {
+    return ("true")
+    } else { 
+        return ("false")
+}
+}
+
+
+

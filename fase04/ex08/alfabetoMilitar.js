b303ffa1cbe102738e249d6b33db9d7776af7697
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-07-23 20:57:23 -0300

    alfabetoMilitar

---
+++ fase04/ex08/alfabetoMilitar.js
@@ -0,0 +1,24 @@
+function alfabetoMilitar(vogal){
+    if (vogal == "a"){
+        return ("alfa") 
+    } else {
+    if (vogal == "e"){
+    return ("echo")
+    } else {
+    if (vogal == "i"){
+    return ("india")
+    } else {
+    if (vogal == "o"){
+    return ("oscar")
+    } else {
+    if (vogal == "u"){
+    return ("uniform")
+    } else {
+        return ("False")
+}
+}
+}
+}
+}
+}
+

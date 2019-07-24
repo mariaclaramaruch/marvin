3d89d43a083694a54b04a1de701ef9214a53e3cd
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-07-24 16:41:05 -0300

    ABCdario

--- fase04/ex04/abc.js
+++ fase04/ex04/abc.js
@@ -1,13 +1,10 @@
-
 function abc(palavra) {
-    var letra=palavra.toString()
-    var inicial= letra.substr(0,1)
-    if (inicial == "a"|| inicial == "b" || inicial == "c") {
-    return ("true")
+    var min = palavra.toLowerCase()
+    var inicial=min.toString()
+    var letra= inicial.substr(0,1)
+    if (letra == "a"|| letra == "b" || letra == "c") {
+    return true
     } else { 
-        return ("false")
+        return false
 }
 }
-
-
-

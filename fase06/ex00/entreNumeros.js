b52daff11ff14bdc7938fb58100cd4be8c7aa44a
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-08-01 20:53:10 -0300

    intervaloEntreNumeros

---
+++ fase06/ex00/entreNumeros.js
@@ -0,0 +1,22 @@
+function entreNumeros(n1,n2){
+var min;
+var max;
+
+if (n1<n2){
+min= n1
+max= n2
+
+} else {
+    
+min= n2
+max= n1
+
+}
+
+while (min <= max){
+console.log (min) 
+min++;
+}
+}
+
+

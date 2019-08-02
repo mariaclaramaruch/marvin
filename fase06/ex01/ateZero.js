0c3d16eccc6afad93289795ac8d5ebb524fd6d96
mariaclaramaruch <52723050+mariaclaramaruch@users.noreply.github.com>
2019-08-01 21:15:29 -0300

    Exercicio para continuar

---
+++ fase06/ex01/ateZero.js
@@ -0,0 +1,31 @@
+function ateZero(num){
+var novaLista=[]
+var x= 0 
+if (num>=0)
+return 
+
+
+var min;
+var max;
+
+if (n1<n2){
+min= n1
+max= n2
+
+} else {
+
+if (n1>n2){
+    
+min= n2
+max= n1
+}
+}
+
+while (min <= max){
+console.log (min) 
+min++;
+}
+}
+
+
+
